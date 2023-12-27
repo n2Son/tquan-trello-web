ec8e9381f21ea1585678311c54e1e84d8645b195
n2Son <sonanh1102.dev@gmail.com>
2023-12-27 11:36:22 +0700

    Add Theme.js

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
 import HomeIcon from '@mui/icons-material/Home'
 import { pink } from '@mui/material/colors'
+import Typography  from '@mui/material/Typography'
 function App() {
 
   return (
     <>
       <div>sonanhDev</div>
+
+      <Typography variant="h1" color="text.secondary">h1</Typography>
+
       <Button variant="text">Text</Button>
       <Button variant="contained">contained</Button>
       <Button variant="outlined">outlined</Button>
